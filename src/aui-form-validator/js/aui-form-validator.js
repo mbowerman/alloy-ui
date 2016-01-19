47d9f63d01a1b49417c456eb7ac694a97047d76e
Thiago Rocha <thiago.rocha@liferay.com>
2016-01-19 14:32:36 -0800

    AUI-2037 Source formatting and test
    Conflicts:
    	src/aui-form-validator/js/aui-form-validator.js

--- src/aui-form-validator/js/aui-form-validator.js
+++ src/aui-form-validator/js/aui-form-validator.js
@@ -357,12 +357,10 @@ var FormValidator = A.Component.create({
 		},
 
 		clearFieldError: function(field) {
-			var instance = this;
-
 			var fieldName = isNode(field) ? field.get('name') : field;
 
             if (isString(fieldName)) {
-                delete instance.errors[fieldName];
+                delete this.errors[fieldName];
             }
 		},
 
@@ -535,18 +533,16 @@ var FormValidator = A.Component.create({
 		},
 
 		resetField: function(field) {
-			var instance = this;
+			var fieldNode,
+                stackContainer;
 
-			instance.clearFieldError(field);
-
-			var fieldNode = isString(field) ? instance.getField(field) : field;
+			this.clearFieldError(field);
+            fieldNode = isString(field) ? this.getField(field) : field;
 
             if (isNode(fieldNode)) {
-                var stackContainer = instance.getFieldStackErrorContainer(fieldNode);
-
+				stackContainer = this.getFieldStackErrorContainer(fieldNode);
                 stackContainer.remove();
-
-                instance.resetFieldCss(fieldNode);
+				this.resetFieldCss(fieldNode);
             }
 		},
 
@@ -595,17 +591,17 @@ var FormValidator = A.Component.create({
 		},
 
 		validateField: function(field) {
-			var instance = this;
+			var fieldNode,
+				validatable;
 
-			instance.resetField(field);
-
-			var fieldNode = isString(field) ? instance.getField(field) : field;
+			this.resetField(field);
+            fieldNode = isString(field) ? this.getField(field) : field;
 
             if (isNode(fieldNode)) {
-                var validatable = instance.validatable(fieldNode);
+                validatable = this.validatable(fieldNode);
 
 				if (validatable) {
-					instance.fire(EV_VALIDATE_FIELD, {
+					this.fire(EV_VALIDATE_FIELD, {
 						validator: {
 							field: fieldNode
 						}
